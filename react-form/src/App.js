0aede317b9fba65cac4122cff08ee5b783a03c4d
Sheschire <theo.lemesle42@gmail.com>
2022-09-16 16:35:39 +0200

    add a React project (form) still needing CORS handling

---
+++ react-form/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PostApiForm from './components/PostApiForm'
+
+function App() {
+  return (
+    <div className="App">
+      <PostApiForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-form/src/components/PostApiForm.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import Axios from 'axios';
+
+
+function PostApiForm() {
+    const url = "https://script.google.com/macros/s/AKfycby-TJmFFUFTfiNUbMoSIZx8LVtiskQ-bUt4xO6hmrU0XQpJS8IPUBow/exec"
+    const [data, setData] = useState({
+        cle: "CLE-TEST-IOT",
+        id: "",
+        date: "",
+        urlRelais: "",
+        message: ""
+    })
+
+    function submit(e){
+        e.preventDefault();
+        Axios.post(url,{
+            cle: data.cle,
+            donnees: {
+                id: data.id,
+                date: data.date,
+                urlRelais: data.urlRelais,
+                message: data.message
+            }
+        })
+        .then(res=>{
+            console.log(res.data)
+        })
+    }
+
+    function handle(e){
+        const newdata={...data}
+        newdata[e.target.id] = e.target.value
+        setData(newdata)
+        console.log(newdata)
+    }
+    return (
+        <div>
+            <form onSubmit={(e)=> submit(e)}>
+                <input onChange={(e)=>handle(e)} id="id" value={data.id} placeholder="id" type="text"></input>
+                <input onChange={(e)=>handle(e)} id="date" value={data.date} placeholder="date" type="date"></input>
+                <input onChange={(e)=>handle(e)} id="urlRelais" value={data.urlRelais} placeholder="urlRelais" type="text"></input>
+                <textarea onChange={(e)=>handle(e)} id="message" value={data.message} placeholder="message" type="text"></textarea>
+                <button>Post to Ouivalo API !</button>
+            </form>
+        </div>
+    );
+}
+
+export default PostApiForm;
